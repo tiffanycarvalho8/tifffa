ef826f477aa16f11184729adcc9beb6dd779913e
tiffanycarvalho8 <168749549+tiffanycarvalho8@users.noreply.github.com>
2024-11-27 09:40:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Série',
+    'Quem morre no final de The Vampire Diares?',
+    'Sthefan'
+)
+
+criaCartao(
+    'Série',
+    'Com quem Blair fica no final de Gossip Girl?',
+    'Chuck Bass'
+)
+
+criaCartao(
+    'Série',
+    'O que acontece com o Rick em TWD?',
+    'Ele some na ponte'
+)
+
+criaCartao(
+    'Série',
+    'Qual é o protagonista da série de Prison Break?',
+    'Michael Scofieltin'
+)
